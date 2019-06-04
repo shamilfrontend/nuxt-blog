069ce9664b9fab9c8e5db0434a5fb9cc3a0f9ed7
= <sh.alisultanov@maximatelecom.ru>
2019-06-04 23:07:21 +0300

    Настройка и установка графиков

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Admin</h1>
+  <div class="">
+    <h1>Аналитика по постам</h1>
+
+    <canvas ref="canvas"></canvas>
+  </div>
 </template>
 
 <script>
-  import AppPost from '@/components/main/Post';
+  import {Bar} from 'vue-chartjs';
 
   export default {
     middleware: ['admin-auth'],
@@ -14,8 +18,44 @@
       title: 'Панель управления',
     },
 
-    components: {
-      AppPost,
+    extends: Bar,
+
+    mounted() {
+      const data = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      };
+      const options = {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      };
+
+      this.renderChart(data, options);
     },
   };
 </script>
